d25a74a7166c020fe5355cdf727351b88f6bc129
xushengfeng <xushengfeng_zg@163.com>
2021-12-20 01:20:24 +0800

    :lipstick:界面操作

--- js/js.js
+++ js/js.js
@@ -1,10 +1,31 @@
-const { ipcRenderer } = require("electron");
-
-document.querySelector("button").onclick = () => {
-    full_screen(true);
-    count_down();
+document.querySelector("#time").oninput = () => {
+    if (document.querySelector("#time").innerText.match(/\D/) != null) {
+        document.querySelector("#time").innerText = "";
+    }
 };
 
+document.querySelector("button").onclick = () => {
+    count_down_ui(true);
+    full_screen(true);
+};
+
+function count_down_ui(v) {
+    if (v) {
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").className = "dis";
+        document.querySelector("#time").className = "time";
+    } else {
+        document.querySelector("button").disabled = false;
+        document.querySelector("button").className = "";
+        document.querySelector("#time").className = "";
+    }
+    set_time = new Date().getTime();
+    count_down()
+}
+
+set_time = null;
+
+const { ipcRenderer } = require("electron");
 function full_screen(v) {
     ipcRenderer.send("full_screen", v);
 }
